c2181840d414d705d75871231016d8e4c81b1dd6
phat dev <phat.tt@digibird.io>
2023-08-07 11:36:46 +0700

    update href doc

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
       <div>
         <a href="https://digibird.io" target="_blank">
           <img src='/public/logo-350x125.png' className="logo" alt="DigiBird logo" />
@@ -15,7 +15,7 @@ function App() {
       </div>
       <h1>DigiBird Test Exercise</h1>
       <div className="card">
-        <a href="" target='_blank'>
+        <a href="https://docs.google.com/document/d/1Xph8cecOCQHhl-sXSWPCPr56S_YG_iubhyO3-BafV4E/edit?usp=sharing" target='_blank'>
           <button>
             Exercise Test 
           </button>
@@ -24,7 +24,7 @@ function App() {
       <p className="read-the-docs">
         Click on the button view details exercise
       </p>
-    </>
+    </div>
   )
 }
 
